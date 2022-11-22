0b21e532c7f3f2d11baf0563852e41707d1ebac5
Fatima Siddiqui <fatimasiddiqui@Fatimas-MacBook-Pro.local>
2022-11-22 13:34:06 -0500

    finished till conclusion

--- flaskr-tdd/tests/app_test.py
+++ flaskr-tdd/tests/app_test.py
@@ -14,9 +14,9 @@ def client():
     app.config["TESTING"] = True
     app.config["DATABASE"] = BASE_DIR.joinpath(TEST_DB)
 
-    init_db() # setup
-    yield app.test_client() # tests run here
-    init_db() # teardown
+    init_db()  # setup
+    yield app.test_client()  # tests run here
+    init_db()  # teardown
 
 
 def login(client, username, password):
@@ -77,10 +77,13 @@ def test_messages(client):
 
 def test_delete_message(client):
     """Ensure the messages are being deleted"""
-    rv = client.get('/delete/1')
+    rv = client.get("/delete/1")
+    data = json.loads(rv.data)
+    assert data["status"] == 0
+    login(client, app.config["USERNAME"], app.config["PASSWORD"])
+    rv = client.get("/delete/1")
     data = json.loads(rv.data)
     assert data["status"] == 1
-
 
 
 @pytest.fixture
